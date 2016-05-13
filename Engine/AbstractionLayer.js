66cfc2a79f9aa93dd6426f104e12313bf132982d
markuspinter <markus.m.pinter@gmail.com>
2016-05-13 21:29:04 +0200

    *init commit

---
+++ Engine/AbstractionLayer.js
@@ -0,0 +1,59 @@
+/**
+ * Created by Markus on 5/13/2016.
+ */
+
+class GameScreen
+{
+    consturctor(width, height)
+    {
+        this.screenWidth = width;
+        this.screenHeight = height;
+    }
+
+    get ScreenWidth()
+    {
+        return this.screenWidth;
+    }
+
+    set ScreenWidth(width)
+    {
+        this.screenWidth = width;
+    }
+
+    get ScreenHeight()
+    {
+        return this.screenHeight;
+    }
+
+    set ScreenHeight(height)
+    {
+        this.screenHeight = height;
+    }
+
+}
+
+class GameBuffer
+{
+    consturctor(buffer)
+    {
+
+    }
+}
+
+function LoadContext()
+{
+    canvas = $("#gameCanvas");
+    ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    console.log(canvas.width);
+    console.log(canvas.height);
+
+    var gameScreen = new GameScreen(canvas.width, canvas.height);
+}
+
+$().ready()
+{
+    LoadContext();
+}
